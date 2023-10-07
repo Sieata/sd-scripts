1da56cfba3bd5a1896fa6687c421dfc1c48baf21
Sieata <sieatas@gmail.com>
2023-10-07 21:34:31 +0800

    Update train_util.py

--- library/train_util.py
+++ library/train_util.py
@@ -3775,7 +3775,7 @@ def prepare_accelerator(args: argparse.Namespace):
     accelerator = Accelerator(
         gradient_accumulation_steps=args.gradient_accumulation_steps,
         mixed_precision=args.mixed_precision,
-        log_with=log_with,
+        # log_with=log_with,
         # project_dir=logging_dir,
     )
     return accelerator
