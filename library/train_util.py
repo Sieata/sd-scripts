e8dec1c18c3c2a5d52fa50929da3dd986b394f52
Sieata <sieatas@gmail.com>
2023-10-07 20:53:51 +0800

    Update train_util.py

--- library/train_util.py
+++ library/train_util.py
@@ -3771,7 +3771,8 @@ def prepare_accelerator(args: argparse.Namespace):
                 os.environ["WANDB_DIR"] = logging_dir
             if args.wandb_api_key is not None:
                 wandb.login(key=args.wandb_api_key)
-
+    
+    print('logging_dir:'+logging_dir)
     accelerator = Accelerator(
         gradient_accumulation_steps=args.gradient_accumulation_steps,
         mixed_precision=args.mixed_precision,
